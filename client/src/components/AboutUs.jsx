fccd98ddbbf96a8338cd462ffb45d48b5a826ff0
Rehan Ganapathy <79349712+rehanganapathy@users.noreply.github.com>
2022-06-17 20:39:07 +0530

    Add files via upload

--- client/src/components/AboutUs.jsx
+++ client/src/components/AboutUs.jsx
@@ -1,26 +1,41 @@
 import React from 'react'
+import logo from '../../images/logo.png'
 
 function AboutUs() {
   return (
-    <div>
-      about decage
+    <div className='min-h-screen gradient-bg-aboutus text-white height-100vh'>
+      <div className='flex w-full justify-center items-center'>
+      <img src = {logo} alt = "logo" className = "w-40 align-content: center"  />
+      </div>
+      <div className='align-content: space-evenly'>
+        About Decage
+      </div>
 
-this is an ethereum based web application where you can make borderless transactions. 
 
-for using this application,
-* make sure metamask extension is installed on your browser
-* you have an account on meta mask 
-* connect your wallet to decage platform to make transactions
+<p><br/>
+This is an ethereum based web application where you can make borderless transactions. 
+</p><br/>
 
-every transaction you make is added to the blockchain with a giphy corresponding to the keyword you give during the transaction
+<p>
+For using this application,
+<br/><br/>
+-&gt; make sure metamask extension is installed on your browser<br/><br/>
+-&gt; you have an account on meta mask <br/><br/>
+-&gt; connect your wallet to decage platform to make transactions<br/><br/>
+</p><br/>
+<p>
+Every transaction you make is added to the blockchain with a giphy corresponding to the keyword you give during the transaction
+</p><br/>
 
+<p>
 crypto enthusiasts behind this
+</p><br/>
 
 we are a team of 3 computer science engineers from PES university who are crypto enthusiasts and have been exploring a lot in this domain past a few years. 
-
-rakshika - (github link)
-rehan - (github link)
-praneeth - (github link)
+<br/><br/>
+rakshika - (github link)<br/>
+rehan - (github link)<br/>
+praneeth - (github link)<br/>
 
     </div>
   )
