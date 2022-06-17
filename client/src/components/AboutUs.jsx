c92e4af1ba660fe8ca87c8ab6fee2bf0095c88ad
Rehan Ganapathy <79349712+rehanganapathy@users.noreply.github.com>
2022-06-17 19:30:34 +0530

    Add files via upload

---
+++ client/src/components/AboutUs.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function AboutUs() {
+  return (
+    <div>
+      about decage
+
+this is an ethereum based web application where you can make borderless transactions. 
+
+for using this application,
+* make sure metamask extension is installed on your browser
+* you have an account on meta mask 
+* connect your wallet to decage platform to make transactions
+
+every transaction you make is added to the blockchain with a giphy corresponding to the keyword you give during the transaction
+
+crypto enthusiasts behind this
+
+we are a team of 3 computer science engineers from PES university who are crypto enthusiasts and have been exploring a lot in this domain past a few years. 
+
+rakshika - (github link)
+rehan - (github link)
+praneeth - (github link)
+
+    </div>
+  )
+}
+
+export default AboutUs;
