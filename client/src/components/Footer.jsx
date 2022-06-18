fb18453d37db080d15f6f02f1806704d1d4039cf
Rehan Ganapathy <79349712+rehanganapathy@users.noreply.github.com>
2022-06-18 14:06:40 +0530

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,29 +1,40 @@
-import React from "react";
+import React,{useContext} from "react";
 import { Link } from "react-router-dom";
 import logo from "../../images/logo.png";
+import { TransactionContext } from "../context/TransactionContext";
 
-const Footer = () => (
-  <div className="w-full flex md:justify-center justify-between items-center flex-col p-4 gradient-bg-footer">
-    <div className="w-full flex sm:flex-row flex-col justify-between items-center my-4">
+
+const Footer = () => {
+  const {currentAccount} = useContext(TransactionContext);
+
+  return(
+
+  <div className="w-full  flex:justify-center justify-between items-center flex-col p-4 gradient-bg-footer">
+    <div className="w-full flex sm:flex-row flex-col justify-between items-center my-4 w-100%">
       
       
     </div>
 
-    <div className="flex justify-center items-center flex-col mt-5">
-      <p className="text-white text-center text-3xl">
-      <div className="pl-5 flex items-center justify-center" >
+    <div className="flex justify-center items-center flex-col mt-5 ">
+      <p className="text-white  text-center text-3xl">
+      <div className="pl-5 flex items-center justify-center w-full" >
         <img src={logo} alt="logo" className="w-40" />
       </div>
        Usher in the next leap of growth </p>
        <br/>
-       <Link to="/about" className="text-white text-center text-3xl">
+      <div className="w-full">
+      <iframe className="w-full h-full" src={`https://rinkeby.etherscan.io/address/${currentAccount}`}>
+            
+            </iframe>
+       </div>
+       <Link to="/about" className="text-white  text-center text-3xl">
         About Us
        </Link>
-       <br />
+       <br/>
       
       <p className="text-white text-sm text-center font-medium mt-2">info@decage.com</p>
     </div>
-
+    
     <div className="sm:w-[90%] w-full h-[0.25px] bg-gray-400 mt-5 " />
 
     <div className="sm:w-[90%] w-full flex justify-between items-center mt-3">
@@ -31,6 +42,8 @@ const Footer = () => (
       <p className="text-white text-right text-xs">All rights reserved</p>
     </div>
   </div>
-);
+
+  )
+};
 
 export default Footer;
