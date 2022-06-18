45f87a2f97c7291a733a3ab8fae374e1a13934cf
Rehan Ganapathy <79349712+rehanganapathy@users.noreply.github.com>
2022-06-18 14:44:30 +0530

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -16,7 +16,7 @@ const Footer = () => {
     </div>
 
     <div className="flex justify-center items-center flex-col mt-5 ">
-      
+     
        <br/>
      {currentAccount &&
       <div className="w-full">
@@ -38,11 +38,11 @@ const Footer = () => {
       <p className="text-white text-sm text-center font-medium mt-2">info@decage.com</p>
     </div>
     
-    <div className="sm:w-[90%] w-full h-[0.25px] bg-gray-400 mt-5 " />
+    <div className=" w-full h-[0.25px] bg-gray-400 mt-5 " />
 
-    <div className="sm:w-[90%] w-full flex justify-between items-center mt-3">
+    <div className=" w-full flex justify-between items-center mt-3">
       <p className="text-white text-left text-xs">@decage2022</p>
-      <p className="text-white text-right text-xs">All rights reserved</p>
+      <p className="text-white text-right text-xs ">All rights reserved</p>
     </div>
   </div>
 
